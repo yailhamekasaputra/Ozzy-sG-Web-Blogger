67a0bf3e40530c8fb8a76c43716c6e3c5b3bb73a
yailhamekasaputra <yailhameka11@gmail.com>
2024-04-04 04:27:56 +0700

    first commit

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  // siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+  siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
   siteLogo: '/logo.png',
   socialBanner: '/social-banner.png', // add social banner in the public folder
   email: 'yailhameka11@gmail.com',

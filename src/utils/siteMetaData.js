18a4aeaa66771068292b089c72c89ba188f897dc
yailhamekasaputra <yailhameka11@gmail.com>
2024-04-03 21:55:56 +0700

    first commit

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,21 +1,21 @@
 // please update required information
 const siteMetadata = {
   title: 'Next.js Blog With Tailwind CSS and Contentlayer',
-  author: 'CodeBucks',
+  author: 'yailhameka',
   headerTitle: 'Next.js Blog',
   description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   // siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
-  // siteLogo: '/logo.png',
-  // socialBanner: '/social-banner.png', // add social banner in the public folder
-  // email: 'codebucks27@gmail.com',
-  // github: 'https://github.com/codebucks27',
-  // twitter: 'https://twitter.com/code_bucks',
-  // facebook: 'https://facebook.com',
-  // youtube: 'https://youtube.com/codebucks',
-  // linkedin: 'https://www.linkedin.com/in/codebucks/',
-  // dribbble: 'https://www.dribbble.com',
+  siteLogo: '/logo.png',
+  socialBanner: '/social-banner.png', // add social banner in the public folder
+  email: 'yailhameka11@gmail.com',
+  github: 'https://github.com/yailhamekasaputra',
+  twitter: 'https://twitter.com/',
+  facebook: 'https://facebook.com',
+  youtube: 'https://youtube.com/',
+  linkedin: 'https://www.linkedin.com',
+  dribbble: 'https://www.dribbble.com',
   locale: 'en-US',
 };
 

63e47ca869ecd3e56c0a606a7610b0d4b9edbb2d
yailhamekasaputra <yailhameka11@gmail.com>
2024-10-19 15:51:02 +0700

    fixed add new featured slider

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Slider from '../components/Home/Slider';
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center md:mt-10">
-      <Slider />
+      <Slider blogs={allBlogs} />
       <HomeCoverSection blogs={allBlogs} />
       <FeaturedPosts blogs={allBlogs} />
       <RecentPosts blogs={allBlogs} />

fbabf8a66ea8f6a3428021fb3d8816b96a95a7f9
yailhamekasaputra <yailhameka11@gmail.com>
2024-10-19 13:23:59 +0700

    fixed position featured posts

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,12 @@ import { allBlogs } from 'contentlayer/generated';
 import HomeCoverSection from '../components/Home/HomeCoverSection';
 import FeaturedPosts from '../components/Home/FeaturedPosts';
 import RecentPosts from '../components/Home/RecentPosts';
+import Slider from '../components/Home/Slider';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center md:mt-10">
+      <Slider />
       <HomeCoverSection blogs={allBlogs} />
       <FeaturedPosts blogs={allBlogs} />
       <RecentPosts blogs={allBlogs} />
--- src/components/Home/FeaturedPosts.js
+++ src/components/Home/FeaturedPosts.js
@@ -6,19 +6,41 @@ import BlogLayoutTwo from '../Blog/BlogLayoutTwo';
 const FeaturedPosts = ({ blogs }) => {
   const sortedBlogs = sortBlogs(blogs);
   return (
-    <section className="w-full mt-16 sm:mt-24  md:mt-32 px-5 sm:px-10 md:px-24  sxl:px-32 flex flex-col items-center justify-center">
+    // <section className="w-full mt-16 sm:mt-24  md:mt-32 px-5 sm:px-10 md:px-24  sxl:px-32 flex flex-col items-center justify-center">
+    //   <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">
+    //     Featured Posts
+    //   </h2>
+
+    //   <div className="grid grid-cols-2 grid-rows-2 gap-6  mt-10 sm:mt-16">
+    //     <article className=" col-span-2  sxl:col-span-1 row-span-2 relative">
+    //       <BlogLayoutOne blog={sortedBlogs[1]} />
+    //     </article>
+    //     <article className=" col-span-2 sm:col-span-1 row-span-1 relative">
+    //       <BlogLayoutTwo blog={sortedBlogs[2]} />
+    //     </article>
+    //     <article className="col-span-2 sm:col-span-1 row-span-1 relative">
+    //       <BlogLayoutTwo blog={sortedBlogs[3]} />
+    //     </article>
+    //   </div>
+    // </section>
+    <section className="w-full mt-16 sm:mt-24 md:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32 flex flex-col items-center justify-center">
       <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl text-dark dark:text-light">
         Featured Posts
       </h2>
 
-      <div className="grid grid-cols-2 grid-rows-2 gap-6  mt-10 sm:mt-16">
-        <article className=" col-span-2  sxl:col-span-1 row-span-2 relative">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-10 sm:mt-16">
+        {/* Artikel pertama yang akan mengambil dua baris */}
+        <article className="col-span-1 sm:col-span-2 md:col-span-1 row-span-2 relative">
           <BlogLayoutOne blog={sortedBlogs[1]} />
         </article>
-        <article className=" col-span-2 sm:col-span-1 row-span-1 relative">
+
+        {/* Artikel kedua yang lebih kecil */}
+        <article className="col-span-1 row-span-1 relative">
           <BlogLayoutTwo blog={sortedBlogs[2]} />
         </article>
-        <article className="col-span-2 sm:col-span-1 row-span-1 relative">
+
+        {/* Artikel ketiga yang lebih kecil */}
+        <article className="col-span-1 row-span-1 relative">
           <BlogLayoutTwo blog={sortedBlogs[3]} />
         </article>
       </div>

83f14b688584661d970b0fa5f4648113149f6af5
yailhamekasaputra <yailhameka11@gmail.com>
2024-10-24 17:44:44 +0700

    revisi layout detail blog

--- src/components/Blog/BlogDetails.js
+++ src/components/Blog/BlogDetails.js
@@ -32,34 +32,26 @@ import { FiCalendar, FiEye, FiClock } from 'react-icons/fi'; // Import icons fro
 
 const BlogDetails = ({ blog, slug: blogSlug }) => {
   return (
-    <div className="px-2 md:px-10 bg-dark dark:bg-white text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5 md:mx-10 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out">
+    <div className=" md:px-10 bg-dark dark:bg-white text-light dark:text-dark py-2 flex items-center justify-around flex-wrap text-lg sm:text-xl font-medium mx-5 md:mx-10 rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out">
       <div className="m-1 flex items-center">
         <FiCalendar className="mr-1" /> {/* Calendar icon */}
-        <span className="text-sm">
+        <span className="text-xs md:text-sm">
           {format(parseISO(blog.publishedAt), 'LLLL d, yyyy')}
         </span>
       </div>
       <div className="m-1 flex items-center">
         <FiEye className="mr-1" /> {/* Eye icon */}
-        <span className="text-sm">
+        <span className="text-xs md:text-sm ">
           <ViewCounter slug={blogSlug} />
         </span>
       </div>
       <div className="m-1 flex items-center">
         <FiClock className="mr-1" /> {/* Clock icon */}
-        <span className="text-sm">{blog.readingTime.text}</span>
+        <span className="text-xs md:text-sm">{blog.readingTime.text}</span>
       </div>
-      <span className="text-sm">
+      <span className="text-xs md:text-sm">
         <Link href={`/categories/${slug(blog.tags[0])}`} passHref>
-          {/* <p
-            className="' py-1.5  md:py-2 px-6  md:px-10  bg-dark dark:bg-white text-light dark:text-dark rounded-full border-2 border-solid  hover:scale-105 transition-all ease duration-200 m-2',
-        props.className,
-        active
-        ? 'bg-dark  dark:bg-light dark:text-dark'
-        : 'bg-light text-dark  dark:text-light'"
-          > */}
           #{blog.tags[0]}
-          {/* </p> */}
         </Link>
       </span>
     </div>

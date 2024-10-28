cee85c335d2151e84f65ee5a4c6c6a82773f687b
yailhamekasaputra <yailhameka11@gmail.com>
2024-10-28 13:31:20 +0700

    revisi blog detail

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -1,3 +1,171 @@
+// import BlogDetails from '@/src/components/Blog/BlogDetails';
+// import RenderMdx from '@/src/components/Blog/RenderMdx';
+// import Tag from '@/src/components/Elements/Tag';
+// import siteMetadata from '@/src/utils/siteMetaData';
+// import { allBlogs } from 'contentlayer/generated';
+// import { slug } from 'github-slugger';
+// import Image from 'next/image';
+// import { notFound } from 'next/navigation';
+
+// export async function generateStaticParams() {
+//   return allBlogs.map((blog) => ({ slug: blog._raw.flattenedPath }));
+// }
+
+// export async function generateMetadata({ params }) {
+//   const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug);
+//   if (!blog) {
+//     return;
+//   }
+
+//   const publishedAt = new Date(blog.publishedAt).toISOString();
+//   const modifiedAt = new Date(blog.updatedAt || blog.publishedAt).toISOString();
+
+//   let imageList = [siteMetadata.socialBanner];
+//   if (blog.image) {
+//     imageList =
+//       typeof blog.image.filePath === 'string'
+//         ? [siteMetadata.siteUrl + blog.image.filePath.replace('../public', '')]
+//         : blog.image;
+//   }
+//   const ogImages = imageList.map((img) => {
+//     return { url: img.includes('http') ? img : siteMetadata.siteUrl + img };
+//   });
+
+//   const authors = blog?.author ? [blog.author] : siteMetadata.author;
+
+//   return {
+//     title: blog.title,
+//     description: blog.description,
+//     openGraph: {
+//       title: blog.title,
+//       description: blog.description,
+//       url: siteMetadata.siteUrl + blog.url,
+//       siteName: siteMetadata.title,
+//       locale: 'en_US',
+//       type: 'article',
+//       publishedTime: publishedAt,
+//       modifiedTime: modifiedAt,
+//       images: ogImages,
+//       authors: authors.length > 0 ? authors : [siteMetadata.author],
+//     },
+//     twitter: {
+//       card: 'summary_large_image',
+//       title: blog.title,
+//       description: blog.description,
+//       images: ogImages,
+//     },
+//   };
+// }
+
+// export default function BlogPage({ params }) {
+//   const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug);
+
+//   if (!blog) {
+//     notFound();
+//   }
+
+//   let imageList = [siteMetadata.socialBanner];
+//   if (blog.image) {
+//     imageList =
+//       typeof blog.image.filePath === 'string'
+//         ? [siteMetadata.siteUrl + blog.image.filePath.replace('../public', '')]
+//         : blog.image;
+//   }
+
+//   const jsonLd = {
+//     '@context': 'https://schema.org',
+//     '@type': 'NewsArticle',
+//     headline: blog.title,
+//     description: blog.description,
+//     image: imageList,
+//     datePublished: new Date(blog.publishedAt).toISOString(),
+//     dateModified: new Date(blog.updatedAt || blog.publishedAt).toISOString(),
+//     author: [
+//       {
+//         '@type': 'Person',
+//         name: blog?.author ? [blog.author] : siteMetadata.author,
+//         url: siteMetadata.twitter,
+//       },
+//     ],
+//   };
+
+//   return (
+//     <>
+//       <script
+//         type="application/ld+json"
+//         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+//       />
+//       <article>
+//         <div className="mb-8 text-center relative w-full h-[70vh] bg-dark">
+//           <div className="w-full z-10 flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+//             {/* <Tag
+//               name={blog.tags[0]}
+//               link={`/categories/${slug(blog.tags[0])}`}
+//               className="px-6 text-sm py-2"
+//             /> */}
+//             <h1 className="inline-block mt-6 font-bold capitalize text-light text-2xl md:text-3xl lg:text-5xl !leading-normal relative w-5/6">
+//               {blog.title}
+//             </h1>
+//           </div>
+//           <div className="absolute top-0 left-0 right-0 bottom-0 h-full bg-dark/60 dark:bg-dark/40" />
+//           <Image
+//             src={blog.image.filePath.replace('../public', '')}
+//             placeholder="blur"
+//             blurDataURL={blog.image.blurhashDataUrl}
+//             alt={blog.title}
+//             width={blog.image.width}
+//             height={blog.image.height}
+//             className="aspect-square w-full h-full object-cover object-center"
+//             priority
+//             sizes="100vw"
+//           />
+//         </div>
+//         <BlogDetails blog={blog} slug={params.slug} />
+
+//         <div className="grid grid-cols-12  gap-y-8 lg:gap-8 sxl:gap-16 mt-8 px-5 md:px-10">
+//           <div className="col-span-12  lg:col-span-4">
+//             <details
+//               className="border-[1px] border-solid border-dark dark:border-light text-dark dark:text-light rounded-lg p-4 sticky top-6 max-h-[80vh] overflow-hidden overflow-y-auto"
+//               open
+//             >
+//               <summary className="text-lg font-semibold capitalize cursor-pointer">
+//                 Table Of Content
+//               </summary>
+//               <ul className="mt-4 font-in text-base">
+//                 {blog.toc.map((heading) => {
+//                   return (
+//                     <li key={`#${heading.slug}`} className="py-1">
+//                       <a
+//                         href={`#${heading.slug}`}
+//                         data-level={heading.level}
+//                         className="data-[level=two]:pl-0  data-[level=two]:pt-2
+//                                        data-[level=two]:border-t border-solid border-dark/40
+//                                        data-[level=three]:pl-4
+//                                        sm:data-[level=three]:pl-6
+//                                        flex items-center justify-start
+//                                        "
+//                       >
+//                         {heading.level === 'three' ? (
+//                           <span className="flex w-1 h-1 rounded-full bg-dark mr-2">
+//                             &nbsp;
+//                           </span>
+//                         ) : null}
+
+//                         <span className="hover:underline">{heading.text}</span>
+//                       </a>
+//                     </li>
+//                   );
+//                 })}
+//               </ul>
+//             </details>
+//           </div>
+//           <RenderMdx blog={blog} />
+//         </div>
+//       </article>
+//     </>
+//   );
+// }
+
 import BlogDetails from '@/src/components/Blog/BlogDetails';
 import RenderMdx from '@/src/components/Blog/RenderMdx';
 import Tag from '@/src/components/Elements/Tag';
@@ -6,6 +174,7 @@ import { allBlogs } from 'contentlayer/generated';
 import { slug } from 'github-slugger';
 import Image from 'next/image';
 import { notFound } from 'next/navigation';
+import React from 'react';
 
 export async function generateStaticParams() {
   return allBlogs.map((blog) => ({ slug: blog._raw.flattenedPath }));
@@ -72,6 +241,7 @@ export default function BlogPage({ params }) {
         : blog.image;
   }
 
+  // JSON-LD Schema
   const jsonLd = {
     '@context': 'https://schema.org',
     '@type': 'NewsArticle',
@@ -89,6 +259,15 @@ export default function BlogPage({ params }) {
     ],
   };
 
+  // Filter blogs with the same tag
+  const relatedBlogs = allBlogs
+    .filter(
+      (otherBlog) =>
+        otherBlog.tags.some((tag) => blog.tags.includes(tag)) &&
+        otherBlog._raw.flattenedPath !== blog._raw.flattenedPath
+    )
+    .slice(0, 5); // Display up to 5 related blogs
+
   return (
     <>
       <script
@@ -98,11 +277,6 @@ export default function BlogPage({ params }) {
       <article>
         <div className="mb-8 text-center relative w-full h-[70vh] bg-dark">
           <div className="w-full z-10 flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            {/* <Tag
-              name={blog.tags[0]}
-              link={`/categories/${slug(blog.tags[0])}`}
-              className="px-6 text-sm py-2"
-            /> */}
             <h1 className="inline-block mt-6 font-bold capitalize text-light text-2xl md:text-3xl lg:text-5xl !leading-normal relative w-5/6">
               {blog.title}
             </h1>
@@ -122,8 +296,8 @@ export default function BlogPage({ params }) {
         </div>
         <BlogDetails blog={blog} slug={params.slug} />
 
-        <div className="grid grid-cols-12  gap-y-8 lg:gap-8 sxl:gap-16 mt-8 px-5 md:px-10">
-          <div className="col-span-12  lg:col-span-4">
+        <div className="grid grid-cols-12 gap-y-8 lg:gap-8 sxl:gap-16 mt-8 px-5 md:px-10">
+          <div className="col-span-12 lg:col-span-4">
             <details
               className="border-[1px] border-solid border-dark dark:border-light text-dark dark:text-light rounded-lg p-4 sticky top-6 max-h-[80vh] overflow-hidden overflow-y-auto"
               open
@@ -132,34 +306,54 @@ export default function BlogPage({ params }) {
                 Table Of Content
               </summary>
               <ul className="mt-4 font-in text-base">
-                {blog.toc.map((heading) => {
-                  return (
-                    <li key={`#${heading.slug}`} className="py-1">
-                      <a
-                        href={`#${heading.slug}`}
-                        data-level={heading.level}
-                        className="data-[level=two]:pl-0  data-[level=two]:pt-2
-                                       data-[level=two]:border-t border-solid border-dark/40
-                                       data-[level=three]:pl-4
-                                       sm:data-[level=three]:pl-6
-                                       flex items-center justify-start
-                                       "
-                      >
-                        {heading.level === 'three' ? (
-                          <span className="flex w-1 h-1 rounded-full bg-dark mr-2">
-                            &nbsp;
-                          </span>
-                        ) : null}
-
-                        <span className="hover:underline">{heading.text}</span>
-                      </a>
-                    </li>
-                  );
-                })}
+                {blog.toc.map((heading) => (
+                  <li key={`#${heading.slug}`} className="py-1">
+                    <a
+                      href={`#${heading.slug}`}
+                      data-level={heading.level}
+                      className="data-[level=two]:pl-0  data-[level=two]:pt-2
+                                 data-[level=two]:border-t border-solid border-dark/40
+                                 data-[level=three]:pl-4
+                                 sm:data-[level=three]:pl-6
+                                 flex items-center justify-start
+                                 "
+                    >
+                      {heading.level === 'three' ? (
+                        <span className="flex w-1 h-1 rounded-full bg-dark mr-2">
+                          &nbsp;
+                        </span>
+                      ) : null}
+                      <span className="hover:underline">{heading.text}</span>
+                    </a>
+                  </li>
+                ))}
               </ul>
             </details>
           </div>
-          <RenderMdx blog={blog} />
+
+          {/* Modified RenderMdx to include "Baca Juga" */}
+          <div className="col-span-12 lg:col-span-8">
+            <RenderMdx blog={blog} />
+
+            {/* "Baca Juga" Section in the Middle of the Article */}
+            {relatedBlogs.length > 0 && (
+              <section className="mt-8 mb-8">
+                <h2 className="text-xl font-semibold mb-4">Baca Juga</h2>
+                <ul className="space-y-4">
+                  {relatedBlogs.map((relatedBlog) => (
+                    <li key={relatedBlog._id}>
+                      <a
+                        href={`/blogs/${relatedBlog._raw.flattenedPath}`}
+                        className="text-primary hover:underline"
+                      >
+                        {relatedBlog.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </section>
+            )}
+          </div>
         </div>
       </article>
     </>

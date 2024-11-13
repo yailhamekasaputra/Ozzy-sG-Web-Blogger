cb2ac3182e29a2b1a9fed6a7aa07b939cf5f30a6
yailhamekasaputra <yailhameka11@gmail.com>
2024-11-13 17:26:33 +0700

    revisi imagesource

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -297,9 +297,9 @@ export default function BlogPage({ params }) {
           {/* Sumber gambar */}
           {blog.imageSource && (
             <div className="mt-2 text-sm text-gray-600 dark:text-gray-400 text-center">
-              <span className="absolute bottom-1 right-1 rounded-lg bg-black/40 px-2 py-1 text-xs text-gray-200">
+              <span className="absolute bottom-1 right-1 rounded-lg bg-black/40 px-2 py-0.5 text-xs text-gray-200">
                 <a
-                  className="text-xs text-white"
+                  className="text-[10px] sm:text-xs text-white"
                   href={blog.imageSource} // Menggunakan URL yang ada pada blog.imageSource
                   target="_blank"
                   rel="noopener noreferrer"

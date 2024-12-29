ae55a12bebec15beefe4d083448f852ddbc8bdec
yailhamekasaputra <yailhameka11@gmail.com>
2024-12-29 18:35:25 +0700

    revisi tag

--- src/components/Elements/Tag.js
+++ src/components/Elements/Tag.js
@@ -7,11 +7,11 @@ const Tag = ({ link = '#', name, ...props }) => {
     <Link
       href={link}
       className={cx(
-        'group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-gray-800/30 backdrop-blur-lg px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-xl hover:shadow-gray-600/50 border border-white/20',
+        'group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-gray-800/30 backdrop-blur-lg px-4 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-xl hover:shadow-gray-600/50 border border-white/20',
         props.className
       )}
     >
-      <span className="text-base">{name}</span>
+      <span className="text-sm">{name}</span>
       <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
         <div className="relative h-full w-10 bg-white/20"></div>
       </div>

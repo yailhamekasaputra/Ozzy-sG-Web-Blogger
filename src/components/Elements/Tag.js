79ccf76aea256d8c7bea319434f84053fbd7e27d
yailhamekasaputra <yailhameka11@gmail.com>
2024-12-29 18:28:10 +0700

    revisi tag

--- src/components/Elements/Tag.js
+++ src/components/Elements/Tag.js
@@ -11,7 +11,7 @@ const Tag = ({ link = '#', name, ...props }) => {
         props.className
       )}
     >
-      <span className="text-lg">{name}</span>
+      <span className="text-base">{name}</span>
       <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
         <div className="relative h-full w-10 bg-white/20"></div>
       </div>
